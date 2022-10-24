5fe7e4d9f0140331a4fba37bf8fafa45980be0c7
beng <beng@justplay.lol>
2022-10-24 10:49:35 +0300

    added Player Animations idle and movement

--- Zelda Like/Assets/Scripts/PlayerMovement.cs
+++ Zelda Like/Assets/Scripts/PlayerMovement.cs
@@ -8,17 +8,32 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _speed;
 
     [SerializeField] private Rigidbody2D _myRigidbody;
+    [SerializeField] private Animator _animator;
     private Vector3 _change;
-    
+    private static readonly int MoveX = Animator.StringToHash("moveX");
+    private static readonly int MoveY = Animator.StringToHash("moveY");
+    private static readonly int Moving = Animator.StringToHash("moving");
+
     private void Update()
     {
         _change = Vector3.zero;
         _change.x = Input.GetAxisRaw("Horizontal");
         _change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
 
+    private void UpdateAnimationAndMove()
+    {
         if (_change != Vector3.zero)
         {
             MoveCharacter();
+            _animator.SetFloat(MoveX, _change.x);
+            _animator.SetFloat(MoveY, _change.y);
+            _animator.SetBool(Moving, true);
+        }
+        else
+        {
+            _animator.SetBool(Moving, false);
         }
     }
 
